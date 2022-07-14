604203e4c22ca1ae381cac6b1edf70e6c3f47db7
ersalomo <95175703+ersalomo@users.noreply.github.com>
2022-07-14 16:11:30 +0700

    menambah file template boostrap dan tampilan login

--- routes/web.php
+++ routes/web.php
@@ -2,37 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
-use App\Models\User;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\LoginHomeController as Home;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('contoh', function () {
-    return 'contoh';
-});
 
-Route::get('about', function () {
-    return view('login');
-});
-
-Route::get('get-data', function () {
-    try {
-        $data  = DB::table('users')->get();
-        return response()->json([
-            'status' => 'success',
-            'data' => $data
-        ]);
-    } catch (Exception $e) {
-        return back();
-    }
-});
+Route::get('home', [Home::class, 'index']);
