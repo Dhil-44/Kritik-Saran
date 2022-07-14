4926787d81b347e5727f1719aedcff03d7f67d04
Dhil-44 <abdillah.fadhil09@gmail.com>
2022-07-14 17:40:22 +0700

    signin

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -27,10 +27,7 @@
 
             <hr class="my-4">
 
-            <button class="btn btn-lg btn-block btn-primary" style="background-color: #dd4b39;"
-              type="submit"><i class="fab fa-google me-2"></i> Sign in with google</button>
-            <button class="btn btn-lg btn-block btn-primary mb-2" style="background-color: #3b5998;"
-              type="submit"><i class="fab fa-facebook-f me-2"></i>Sign in with facebook</button>
+           
 
           </div>
         </div>
