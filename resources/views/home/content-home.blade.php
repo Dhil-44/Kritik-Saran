2cf8edb1423bca83195cc5a2a9fdb25ed72f529e
matt1412-arky <79315420+matt1412-arky@users.noreply.github.com>
2022-07-16 11:37:05 +0700

    Last update same with branch ersalomo

--- resources/views/home/content-home.blade.php
+++ resources/views/home/content-home.blade.php
@@ -8,15 +8,13 @@
                 d="M0,256L30,224C60,192,120,128,180,117.3C240,107,300,149,360,165.3C420,181,480,171,540,186.7C600,203,660,245,720,224C780,203,840,117,900,117.3C960,117,1020,203,1080,208C1140,213,1200,139,1260,117.3C1320,96,1380,128,1410,144L1440,160L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z">
             </path>
         </svg> --}}
-        
     <div class="mask flex-center" style="background-color : rgba(11,102,35,0.7); height:250px;">
         <div class="container">
-            
+
             <div class="white-text text-center py-4" style="visibility: visible; animation-name: fadeInUp;">
 
                 <img src="http://cac.kalbis.ac.id/frontend/img/logos/logo kalbis putih.png" alt="thumbnail"
                     class="mx-auto d-block" style="width: 100px">
-                <link rel="shortcut icon" href="{{ asset ('dist/img/image/icon-logo.jpg') }}"> 
                 <h1 class="text-center font-weight-bold">
                     <strong>Kalbis Critics and Suggestions</strong>
                 </h1>
