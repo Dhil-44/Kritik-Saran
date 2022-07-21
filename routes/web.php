9b61f23d5090f97e4be3617022ba9c69be52f0e2
ersalomo <95175703+ersalomo@users.noreply.github.com>
2022-07-21 14:51:19 +0700

    admin/backend

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginHomeController as Home;
 use Brian2694\Toastr\Facades\Toastr;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return redirect()->route('home');
@@ -21,12 +11,12 @@ Route::get('/', function () {
 
 Route::middleware(['guest:web'])->group(function () {
     Route::get('home', [Home::class, 'index'])->name('home');
-    Route::get('home-admin', function () {
-        return view('admin.home.admin-page');
-    })->name('admin/home');
 });
 
 Route::middleware(['auth:web'])->group(function () {
+    Route::get('home-admin', function () {
+        return view('admin.home.admin-page');
+    })->name('admin/home');
     Route::view('home-page', 'user.home.content-home')->name('home-page');
     Route::view('profile', 'user.home.profile')->name('profile');
     Route::view('critics', 'user.home.kategori.critics')->name('critics');
