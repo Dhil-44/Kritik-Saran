c198f5d617f86683675e0bbf1cd49e60f77d4f08
ersalomo <95175703+ersalomo@users.noreply.github.com>
2022-07-22 22:57:24 +0700

    middleware admin

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,23 @@ Route::middleware(['guest:web'])->group(function () {
 });
 
 Route::middleware(['auth:web'])->group(function () {
-    Route::get('home-admin', function () {
-        return view('admin.home.admin-page');
-    })->name('admin/home');
     Route::view('home-page', 'user.home.content-home')->name('home-page');
     Route::view('profile', 'user.home.profile')->name('profile');
     Route::view('critics', 'user.home.kategori.critics')->name('critics');
     Route::view('suggestions', 'user.home.kategori.suggestion')->name('suggestions');
     Route::view('announcements', 'user.home.kategori.annoucements')->name('announcements');
     Route::get('signout', [Home::class, 'signout'])->name('signout');
+    Route::view('user-tables', 'admin.data.user-table')->name('user-tables');
+    Route::view('submission-tables', 'admin.data.submission-table')->name('submission-tables');
+});
+// admin
+Route::middleware(['auth:web'])->group(function () {
+});
+
+Route::group(['prefix' => '/', 'middleware' => ['is_admin']], function () {
+    Route::get('home-admin', function () {
+        return view('admin.home.admin-page');
+    })->name('admin/home');
     Route::view('user-tables', 'admin.data.user-table')->name('user-tables');
     Route::view('submission-tables', 'admin.data.submission-table')->name('submission-tables');
 });
