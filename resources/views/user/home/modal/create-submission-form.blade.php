6d36915b4c9c700ee62be47840c14f69933341ac
ersalomo <95175703+ersalomo@users.noreply.github.com>
2022-07-25 11:23:40 +0700

    admin page

--- resources/views/user/home/modal/create-submission-form.blade.php
+++ resources/views/user/home/modal/create-submission-form.blade.php
@@ -6,14 +6,13 @@
                 <h5 class="modal-title">Submission Form</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-
             <form wire:submit.prevent="createSubmission()">
                 <div class="modal-body">
                     <div class="form-group mb-3 row">
                         <label class="form-label col-3 col-form-label">Assign to</label>
                         <div class="col">
                             <select class="form-select" wire:model="id_user_target">
-                                <option>Choose</option>
+                                <option>To</option>
                                 @foreach ($users as $user)
                                     <option value="{{ $user->id }}">{{ $user->email }}</option>
                                 @endforeach
@@ -31,7 +30,7 @@
                         <label class="form-label col-3 col-form-label">Category</label>
                         <div class="col">
                             <select class="form-select" wire:model="id_cat">
-                                <option>--Choose category--</option>
+                                <option>choose</option>
                                 @foreach ($categories as $ct)
                                     <option value="{{ $ct->id }}"> {{ $ct->name_cat }}</option>
                                 @endforeach
@@ -46,7 +45,7 @@
                     <div class="form-group mb-3 row">
                         <label class="form-label col-3 col-form-label">Message</label>
                         <div class="col">
-                            <textarea wire:model="message" id="" rows="5" class="form-control"></textarea>
+                            <textarea wire:model="message" id="summernote" rows="5" class="form-control"></textarea>
                         </div>
                     </div>
 
@@ -63,31 +62,31 @@
                     {{-- <button type="button" class="btn me-auto" data-bs-dismiss="modal">Close</button> --}}
                     <button type="submit" class="btn btn-green">Submit</button>
                 </div>
+
             </form>
         </div>
     </div>
 </div>
-@push('scripts')
+@push('name')
     <script>
-        // @formatter:off
-        document.addEventListener("DOMContentLoaded", function() {
-            tinyMCE.init({
-                selector: '#tinymce-mytextarea',
-                height: 300,
-                menubar: false,
-                statusbar: false,
-                plugins: [
-                    'advlist autolink lists link image charmap print preview anchor',
-                    'searchreplace visualblocks code fullscreen',
-                    'insertdatetime media table paste code help wordcount'
-                ],
-                toolbar: 'undo redo | formatselect | ' +
-                    'bold italic backcolor | alignleft aligncenter ' +
-                    'alignright alignjustify | bullist numlist outdent indent | ' +
-                    'removeformat',
-                content_style: 'body { font-family: -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif; font-size: 14px; -webkit-font-smoothing: antialiased; }'
-            });
-        })
-        // @formatter:on
+        $('textarea#summernote').summernote({
+            placeholder: 'Hello bootstrap 4',
+            tabsize: 2,
+            height: 100,
+            toolbar: [
+                ['style', ['style']],
+                ['font', ['bold', 'italic', 'underline', 'clear']],
+                // ['font', ['bold', 'italic', 'underline', 'strikethrough', 'superscript', 'subscript', 'clear']],
+                //['fontname', ['fontname']],
+                // ['fontsize', ['fontsize']],
+                ['color', ['color']],
+                ['para', ['ul', 'ol', 'paragraph']],
+                ['height', ['height']],
+                ['table', ['table']],
+                ['insert', ['link', 'picture', 'hr']],
+                //['view', ['fullscreen', 'codeview']],
+                ['help', ['help']]
+            ],
+        });
     </script>
 @endpush
--- resources/views/user/home/tampilan-utama.blade.php
+++ resources/views/user/home/tampilan-utama.blade.php
@@ -6,8 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="http://cdn.bootcss.com/toastr.js/latest/css/toastr.min.css">
     <link href="{{ asset('dist/css/tabler.css') }}" rel="stylesheet">
+    <link href="{{ asset('dist/css/tabler.min.css') }}" rel="stylesheet">
+
+
     <link rel="shortcut icon" href="{{ asset('dist/img/image/logo-kalbis.jpg') }}" type="image/x-icon" />
     @livewireStyles
     <style>
@@ -20,12 +22,11 @@
 <body>
     {{-- @include('header-footer.navbar') --}}
     @yield('content')
-
-    <div class="container">
-    </div>
-    {!! Toastr::message() !!}
-    <script src="{{ asset('dist/js/tabler.min.js') }}"></script>
+    {{-- <script src="{{ asset('dist/libs/tycy/tynymce.js') }}" defer></script> --}}
     <script src="{{ asset('dist/js/jquery.js') }}"></script>
+    <script src="{{ asset('dist/js/tabler.js') }}"></script>
+    <script src="{{ asset('dist/js/tabler.min.js') }}"></script>
+
     @stack('scripts')
     @livewireScripts
 </body>
