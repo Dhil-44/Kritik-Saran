dc8abc75ff228ef6580df460a6c04a3602b00203
ersalomo <95175703+ersalomo@users.noreply.github.com>
2022-07-26 15:05:54 +0700

    ok

--- storage/framework/views/28207491c9ed51867fb0f46019743524842670e7.php
+++ storage/framework/views/28207491c9ed51867fb0f46019743524842670e7.php
@@ -1,17 +1,18 @@
+
 <?php $__env->startSection('title', 'User Tables'); ?>
 <?php $__env->startSection('content'); ?>
     <?php
 if (! isset($_instance)) {
     $html = \Livewire\Livewire::mount('admin.dashboard-admin')->html();
-} elseif ($_instance->childHasBeenRendered('WSbIv1q')) {
-    $componentId = $_instance->getRenderedChildComponentId('WSbIv1q');
-    $componentTag = $_instance->getRenderedChildComponentTagName('WSbIv1q');
+} elseif ($_instance->childHasBeenRendered('sPXs5LX')) {
+    $componentId = $_instance->getRenderedChildComponentId('sPXs5LX');
+    $componentTag = $_instance->getRenderedChildComponentTagName('sPXs5LX');
     $html = \Livewire\Livewire::dummyMount($componentId, $componentTag);
-    $_instance->preserveRenderedChild('WSbIv1q');
+    $_instance->preserveRenderedChild('sPXs5LX');
 } else {
     $response = \Livewire\Livewire::mount('admin.dashboard-admin');
     $html = $response->html();
-    $_instance->logRenderedChild('WSbIv1q', $response->id(), \Livewire\Livewire::getRootElementTagName($html));
+    $_instance->logRenderedChild('sPXs5LX', $response->id(), \Livewire\Livewire::getRootElementTagName($html));
 }
 echo $html;
 ?>
