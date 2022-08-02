a4600813043f2656edae46c35fa0753db841e9b0
ersalomo <95175703+ersalomo@users.noreply.github.com>
2022-08-02 10:59:00 +0700

    Menambah File model Feed

--- storage/framework/views/3f7431a601403b9e5bf399c4c6d9a47e4471b4ca.php
+++ storage/framework/views/3f7431a601403b9e5bf399c4c6d9a47e4471b4ca.php
@@ -1,37 +1,24 @@
-
 <?php $__env->startSection('title', 'Kalbis | Critics and Suggestions'); ?>
 <?php $__env->startSection('content'); ?>
-    <?php echo $__env->make('user.header-footer.navbar', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-    
-    <div class="mask flex-center" style="background-size:cover; height:300px; background:rgb(33, 109, 50)">
-        <div class="container">
-            <div class="white-text text-center py-5" style="visibility: visible; animation-name: fadeInUp;">
-                <img src="http://cac.kalbis.ac.id/frontend/img/logos/logo kalbis putih.png" alt="thumbnail"
-                    class="mx-auto d-block" style="width: 100px">
-                
-                
-                <h1 class="text-center font-weight-bold">
-                    <strong>Kalbis Critics and Suggestions</strong>
-                </h1>
-            </div>
+<?php echo $__env->make('user.header-footer.navbar', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+<div class="mask flex-center" style="background-size:cover; height:300px; background:rgb(33, 109, 50)">
+    <div class="container">
+        <div class="white-text text-center py-5" style="visibility: visible; animation-name: fadeInUp;">
+            <img src="http://cac.kalbis.ac.id/frontend/img/logos/logo kalbis putih.png" alt="thumbnail" class="mx-auto d-block" style="width: 100px">
+
+
+            <h1 class="text-center font-weight-bold">
+                <strong>Kalbis Critics and Suggestions</strong>
+            </h1>
         </div>
     </div>
+</div>
 
-    
-    <?php
-if (! isset($_instance)) {
+
+<?php
+if (!isset($_instance)) {
     $html = \Livewire\Livewire::mount('home.home-page')->html();
-<<<<<<< HEAD
-} elseif ($_instance->childHasBeenRendered('vgOa7yo')) {
-    $componentId = $_instance->getRenderedChildComponentId('vgOa7yo');
-    $componentTag = $_instance->getRenderedChildComponentTagName('vgOa7yo');
-    $html = \Livewire\Livewire::dummyMount($componentId, $componentTag);
-    $_instance->preserveRenderedChild('vgOa7yo');
-} else {
-    $response = \Livewire\Livewire::mount('home.home-page');
-    $html = $response->html();
-    $_instance->logRenderedChild('vgOa7yo', $response->id(), \Livewire\Livewire::getRootElementTagName($html));
-=======
 } elseif ($_instance->childHasBeenRendered('P4QIBsR')) {
     $componentId = $_instance->getRenderedChildComponentId('P4QIBsR');
     $componentTag = $_instance->getRenderedChildComponentTagName('P4QIBsR');
@@ -41,11 +28,10 @@ if (! isset($_instance)) {
     $response = \Livewire\Livewire::mount('home.home-page');
     $html = $response->html();
     $_instance->logRenderedChild('P4QIBsR', $response->id(), \Livewire\Livewire::getRootElementTagName($html));
->>>>>>> 5794835d898ecfd75e853a3ffc2a5da45c03e84b
 }
 echo $html;
 ?>
-    <?php echo $__env->make('user.header-footer.footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php echo $__env->make('user.header-footer.footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
 
 <?php $__env->stopSection(); ?>
 
