b971bbc797137fc83d52556bf80061220ab219eb
ersalomo <95175703+ersalomo@users.noreply.github.com>
2022-08-10 18:50:42 +0700

    CRUD private, public , news

--- resources/views/user/home/news-page.blade.php
+++ resources/views/user/home/news-page.blade.php
@@ -14,33 +14,8 @@
                 </div>
             </div>
         </div>
-        <div class="page-body">
-            <div class="container-xl">
-                <div class="row justify-content-center">
-                    <div class="col-3 mt-4">
-                        <div class="list-group">
-                            @if(\Illuminate\Support\Facades\Auth::user()->role != 'user')
-                                <button class="btn btn-success">
-
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-edit" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                        <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1"></path>
-                                        <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z"></path>
-                                        <path d="M16 5l3 3"></path>
-                                    </svg>
-                                    Create News
-                                </button>
-                            @endif
-
-                        </div>
-                    </div>
-                    <div class="col-lg-10 col-xl-9">
-                        @livewire('news.news')
-                    </div>
-                </div>
-            </div>
-        </div>
+            @livewire('news.news')
     </div>
     @include('user.header-footer.footer')
-
 @endsection
+
