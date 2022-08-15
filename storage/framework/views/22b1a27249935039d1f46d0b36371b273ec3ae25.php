770f75490b8f63ed4a344e99754be56e50180975
matt1412-arky <79315420+matt1412-arky@users.noreply.github.com>
2022-08-15 15:52:09 +0700

    update

--- storage/framework/views/22b1a27249935039d1f46d0b36371b273ec3ae25.php
+++ storage/framework/views/22b1a27249935039d1f46d0b36371b273ec3ae25.php
@@ -6,10 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php echo $__env->yieldContent('title'); ?></title>
-    <link href="<?php echo e(asset('dist/css/tabler.css')); ?>" rel="stylesheet">
     <link href="<?php echo e(asset('dist/css/tabler.min.css')); ?>" rel="stylesheet">
+    <link href="<?php echo e(asset('dist/css/tabler.css')); ?>" rel="stylesheet">
+    <link href="<?php echo e(asset('dist/libs/iJabo/ijabo.min.css')); ?>" rel="stylesheet"></link>
     <link rel="shortcut icon" href="<?php echo e(asset('dist/img/image/icon-logo.jpg')); ?>" type="image/x-icon"/>
-    <script src="<?php echo e(asset('dist/libs/iJabo/ijabo.min.css')); ?>"></script>
     <?php echo \Livewire\Livewire::styles(); ?>
 
 </head>
@@ -17,16 +17,15 @@
 
 <?php echo $__env->yieldContent('content'); ?>
 <script src="<?php echo e(asset('dist/js/jquery.js')); ?>"></script>
+<script src="<?php echo e(asset('dist/libs/iJabo/ijabo.min.js')); ?>"></script>
 <script src="<?php echo e(asset('dist/js/tabler.js')); ?>"></script>
 <script src="<?php echo e(asset('dist/js/tabler.min.js')); ?>"></script>
-<script src="<?php echo e(asset('dist/libs/iJabo/ijabo.min.js')); ?>"></script>
 
 <?php echo $__env->yieldPushContent('scripts'); ?>
 <?php echo \Livewire\Livewire::scripts(); ?>
 
 <script>
     window.addEventListener("showToastr", function (event) {
-        alert('adadd')
         toastr.remove();
         if (event.detail.type === 'info') {
             toastr.info(event.detail.message)
