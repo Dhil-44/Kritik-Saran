fa9bc069185035ae4cb4c6bdca6ccdca75359f6e
ersalomo <95175703+ersalomo@users.noreply.github.com>
2022-08-16 10:21:54 +0700

    edit user

--- resources/views/admin/home/modal/edit-user.blade.php
+++ resources/views/admin/home/modal/edit-user.blade.php
@@ -60,6 +60,7 @@
                         @enderror
                     </div>
 
+
                     <div class="form-group">
                         <div class="row">
                             <div class="col">
