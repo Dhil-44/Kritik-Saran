2687fc29faffdc1768109ba4b0a4ef452f02f54b
ersalomo <95175703+ersalomo@users.noreply.github.com>
2022-08-22 16:39:50 +0700

    gitnck main

--- resources/views/user/header-footer/navbar.blade.php
+++ resources/views/user/header-footer/navbar.blade.php
@@ -12,24 +12,24 @@
             </div>
             <div class="col-lg-6">
                 <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ms-auto fs-4">
+                    <ul class="navbar-nav ms-auto">
                         <li class="nav-item ">
                             <a class="nav-link {{ request()->is('home-page') ? 'active bg-dark text-white py-0 ' : '' }}"
                                 aria-current="page" href="{{ route('home-page') }}">
-                                <h4 class="mt-2 text-uppercase">Home</h4>
+                                <h4 class="mt-2 fs-5 text-uppercase">Home</h4>
                             </a>
                         </li>
                         <li class="nav-item ">
                             <a class="nav-link {{ request()->is('news-page') ? 'active bg-dark text-white py-0 ' : '' }}"
                                 aria-current="page" href="{{ route('news-page') }}">
-                                <h4 class="mt-2 text-uppercase">News</h4>
+                                <h4 class="mt-2 fs-5 text-uppercase">News</h4>
                             </a>
                         </li>
                         @if (auth('web')->user()->role != 'admin')
                             <li class="nav-item status">
                                 <a class="nav-link {{ request()->is('inbox') ? 'active bg-dark text-white py-0' : '' }}"
                                     href="{{ route('inbox.message') }}">
-                                    <h4 class="mt-2 position-relative">Inbox
+                                    <h4 class="mt-2 fs-5 position-relative">Inbox
                                         <span class="badge badge-pill bg-red position-relative mb-2"></span>
                                     </h4>
                                 </a>
@@ -39,14 +39,14 @@
                             <li class="nav-item">
                                 <a class="nav-link text-dark border-success py-0 ms-1"
                                     href="{{ route('admin/home') }}#">
-                                    <h4 class="mt-2 text-uppercase">Backend
+                                    <h4 class="mt-2 text-uppercase fs-5">Backend
                                     </h4>
                                 </a>
                             </li>
                         @endif
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('signout') }}#">
-                                <h4 class="mt-2 text-uppercase">Sign out</h4>
+                                <h4 class="mt-2 fs-5 text-uppercase">Sign out</h4>
                             </a>
                         </li>
                     </ul>
